29efff480d1159e325d1690f0deab4f5d01eb22a
sayfutdiinov <sayfutdinovsaman.com>
2022-11-03 15:27:19 +0500

    First commit

--- js/main.js
+++ js/main.js
@@ -1,72 +1,145 @@
-var elContiner=document.querySelector(".container");
-var domList=document.createElement("ul");
+var domList = document.querySelector(".domList");
+let elTemplate=document.querySelector(".item-tempale").content
+
+//Dom Search element
+const elFormSearch=document.querySelector(".search-form");
+const elFormSearchInput=elFormSearch.querySelector(".form-control")
+const elFormSelect=document.querySelector(".form-select");
+const elFormselectType=document.querySelector(".form-select-type");
+const elSortBySelec=document.querySelector(".sort-by");
+
+// For fragment dom element
+var fragment =new DocumentFragment()
+let selectFragment=new DocumentFragment()
+let selectFragmentType=new DocumentFragment()
+let selectFragmentheight=new DocumentFragment()
+
+let weaknessesArray=[]
+let typeCate=[]
+let heigthArray=[]
+
+function makeList(array) {
+    domList.innerHTML=""
+    for (var pokemon of array) {
+        let tempaleClone=elTemplate.cloneNode(true);
+
+        tempaleClone.querySelector(".topNumber").textContent = pokemon.num
+        tempaleClone.querySelector(".itemImg").dataset.id = pokemon.id
+        tempaleClone.querySelector(".itemName").textContent = pokemon.name
+        tempaleClone.querySelector(".itemCandy").textContent = pokemon.candy
+        tempaleClone.querySelector(".itemImg").src = pokemon.img
+        tempaleClone.querySelector(".itemTime").dateTime = pokemon.spawn_time
+        tempaleClone.querySelector(".itemTime").textContent = pokemon.spawn_time
 
 
-
-console.log(pokemons);
-
-for (var pokemon of pokemons) {
-    
-
-
-
-    var listItem=document.createElement("li");
-
-    var topNumber=document.createElement("span");
-
-    var listTopBox=document.createElement("div");
-    var itemName=document.createElement("h3");
-
-    var itemImg=document.createElement("img");
-
-    var itemBottombox=document.createElement("div");
-    var itemCandy=document.createElement("div");
-    var itemTime=document.createElement("time");
-    
-    
-    domList.className='row justify-content-center p-0 gap-3 ' 
+        tempaleClone.querySelector(".text-weaknesses").textContent=`Weaknesses: ${pokemon.weaknesses}`
+        tempaleClone.querySelector(".type-pokemon").textContent=`Type: ${pokemon.type}`
+        tempaleClone.querySelector(".height-pokemon").textContent = `Heigth: ${pokemon.height} m`
+        tempaleClone.querySelector(".weight-pokemon").textContent = `Weight: ${pokemon.weight} kg`
+        tempaleClone.querySelector(".candy_count-pokemon").textContent = `Candy count: ${pokemon.candy_count || "Not"}`
+        tempaleClone.querySelector(".egg-pokemon ").textContent = `Egg: ${pokemon.egg}`
 
 
-    listItem.className='bg-info col-2 d-flex flex-column rounded-3 border-2 border-dark list-unstyled align-items-center p-0 position-relative' 
-    
-    topNumber.className='position-absolute bg-danger text-warning px-1 rounded-3 top-0 start-100 translate-middle'
-    
-    itemName.className='text-warning py-1 px-1 mt-3 bg-danger rounded-4'
-
-    itemBottombox.classList='p-3 text-white text-center d-flex flex-column'
-    
-    itemCandy.className='d-inline w-auto text-dark '
-    
-    topNumber.textContent=pokemon.num
-
-
-    itemName.textContent=pokemon.name;
-    itemCandy.textContent=pokemon.candy
-    itemImg.src=pokemon.img;
-    itemTime.dateTime=pokemon.spawn_time
-    itemTime.textContent=pokemon.spawn_time
-    itemImg.width="150";
-    itemImg.height="200";
-
-
-
-    
-    elContiner.appendChild(domList);
-    domList.appendChild(listItem);
-
-    listItem.appendChild(topNumber)
-
-    listItem.appendChild(listTopBox)
-    listTopBox.appendChild(itemName);
-    
-    listItem.appendChild(itemImg);
-
-    listItem.appendChild(itemBottombox)
-    itemBottombox.appendChild(itemTime);
-    itemBottombox.appendChild(itemCandy);
-
-
-
+        fragment.appendChild(tempaleClone)
+    }
+    domList.appendChild(fragment)
 }
 
+pokemons = pokemons.map(item=>{
+    return {
+        id: 2,
+		num: item.num,
+		name: item.name,
+		img: item.img,
+		type: item.type,
+		height: Number(item.height.split(" ")[0]),
+		weight: Number(item.weight.split(" ")[0]),
+		candy: item.candy,
+		candy_count: item.candy_count,
+		egg: item.egg,
+		spawn_chance: item.spawn_chance,
+		avg_spawns: item.avg_spawns,
+		spawn_time: item.spawn_time,
+		multipliers: item.multipliers,
+		weaknesses: item.weaknesses,
+		prev_evolution: item.prev_evolution,
+		next_evolution: item.next_evolution
+    }
+})
 
+function filletSerachElm(item) {
+    return pokemons.filter(elm=>{
+        return elm.name.match(item) && (elFormSelect.value === "All" || elm.weaknesses.includes(elFormSelect.value)) && (elFormselectType.value==="All" || elm.type.includes(elFormselectType.value))
+    })
+}
+
+function sortBy(array, whichBy){
+    if (whichBy==="a-z") {
+        array.sort((a, b) => a.name.charCodeAt(0) - b.name.charCodeAt(0))
+    }else if(whichBy=="z-a"){
+        array.sort((a, b) => b.name.charCodeAt(0) - a.name.charCodeAt(0))
+    }else if(whichBy==="default"){
+        return 0
+    }else if(whichBy=="m"){
+        array.sort((a, b) => b.height - a.height)
+    }else if(whichBy=="kg"){
+        array.sort((a, b) => b.weight - a.weight)
+    }else{
+        return 0
+    }
+}
+
+function findWeaknesses(){
+    pokemons.forEach(item=>{
+        let candies=item.weaknesses
+        let typePok=item.type
+        candies.forEach(elm=>{
+            if (!weaknessesArray.includes(elm)) {
+                weaknessesArray.push(elm)
+            }
+        })
+
+        typePok.forEach(i=>{
+            if (!typeCate.includes(i)) {
+                typeCate.push(i)
+            }
+        })
+    })
+}
+ 
+function createOption() {
+    weaknessesArray.forEach(item=>{
+        let forEloption=document.createElement("option");
+        forEloption.textContent=item
+        forEloption.value=item
+        selectFragment.appendChild(forEloption)
+    })
+    elFormSelect.appendChild(selectFragment)
+
+    typeCate.forEach(elm=>{
+        let forTypeOp=document.createElement("option");
+        forTypeOp.textContent=elm;
+        forTypeOp.value=elm
+        selectFragmentType.appendChild(forTypeOp)
+    })
+    elFormselectType.appendChild(selectFragmentType)
+}
+
+elFormSearch.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    let inputValue = new RegExp(elFormSearchInput.value.trim(), 'gi')
+    let newArrayFilter=filletSerachElm(inputValue)
+
+
+    if (newArrayFilter.length > 0){
+
+        sortBy(newArrayFilter, elSortBySelec.value)
+        makeList(newArrayFilter)
+    }else{
+        alert("Sory i can't find this pokemon")
+    }
+})
+
+findWeaknesses()
+createOption()
+makeList(pokemons)
